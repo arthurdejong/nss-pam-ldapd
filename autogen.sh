8fc9b6321e4b770f764a93f9f84933a8f01f2cdc
Arthur de Jong <arthur@arthurdejong.org>
2006-10-11 13:58:45 +0000

    first step in cleaning up build process (switch to newer autoconf/automake and remove generated files from version control)
    git-svn-id: http://arthurdejong.org/svn/nss-pam-ldapd/libnss_ldapd@6 ef36b2f9-881f-0410-afb5-c4e39611909c

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,31 @@
-#!/bin/sh
-aclocal14
-automake14
-autoheader213
-autoconf213
+#! /bin/sh
+
+# run this to generate all initial .in files etc
+
+# copy some files from automake/autoconf into place
+[ -r /usr/share/misc/config.sub ] && \
+  cp -f /usr/share/misc/config.sub config.sub
+[ -r /usr/share/misc/config.guess ] && \
+  cp -f /usr/share/misc/config.guess config.guess
+[ -r /usr/share/automake-1.9/mkinstalldirs ] && \
+  cp -f /usr/share/automake-1.9/mkinstalldirs mkinstalldirs
+[ -r /usr/share/automake-1.9/missing ] && \
+  cp -f /usr/share/automake-1.9/missing missing
+[ -r /usr/share/automake-1.9/install-sh ] && \
+  cp -f /usr/share/automake-1.9/install-sh install-sh
+[ -r /usr/share/automake-1.9/depcomp ] && \
+  cp -f /usr/share/automake-1.9/depcomp depcomp
+[ -r /usr/share/automake-1.9/INSTALL ] && \
+  cp -f /usr/share/automake-1.9/INSTALL INSTALL
+
+# generate aclocal.m4 from configure.ac
+aclocal-1.9
+
+# generate config.h.in from configure.ac
+autoheader --warnings=all
+
+# generate Makefile.in from Makefile.am and configure.ac
+automake-1.9 --warnings=all --add-missing --copy --force-missing
+
+# generate configure from configure.ac
+autoconf --warnings=all
