d93e878bfbe37de87638802760cd825e19b2a5e7
Arthur de Jong <arthur@arthurdejong.org>
2007-08-27 19:37:36 +0000

    upgrade to using automake 1.10
    git-svn-id: http://arthurdejong.org/svn/nss-pam-ldapd/nss-ldapd@370 ef36b2f9-881f-0410-afb5-c4e39611909c

--- autogen.sh
+++ autogen.sh
@@ -7,25 +7,25 @@
   cp -f /usr/share/misc/config.sub config.sub
 [ -r /usr/share/misc/config.guess ] && \
   cp -f /usr/share/misc/config.guess config.guess
-[ -r /usr/share/automake-1.9/mkinstalldirs ] && \
-  cp -f /usr/share/automake-1.9/mkinstalldirs mkinstalldirs
-[ -r /usr/share/automake-1.9/missing ] && \
-  cp -f /usr/share/automake-1.9/missing missing
-[ -r /usr/share/automake-1.9/install-sh ] && \
-  cp -f /usr/share/automake-1.9/install-sh install-sh
-[ -r /usr/share/automake-1.9/depcomp ] && \
-  cp -f /usr/share/automake-1.9/depcomp depcomp
-[ -r /usr/share/automake-1.9/INSTALL ] && \
-  cp -f /usr/share/automake-1.9/INSTALL INSTALL
+[ -r /usr/share/automake-1.10/mkinstalldirs ] && \
+  cp -f /usr/share/automake-1.10/mkinstalldirs mkinstalldirs
+[ -r /usr/share/automake-1.10/missing ] && \
+  cp -f /usr/share/automake-1.10/missing missing
+[ -r /usr/share/automake-1.10/install-sh ] && \
+  cp -f /usr/share/automake-1.10/install-sh install-sh
+[ -r /usr/share/automake-1.10/depcomp ] && \
+  cp -f /usr/share/automake-1.10/depcomp depcomp
+[ -r /usr/share/automake-1.10/INSTALL ] && \
+  cp -f /usr/share/automake-1.10/INSTALL INSTALL
 
 # generate aclocal.m4 from configure.ac
-aclocal-1.9 -I m4
+aclocal-1.10 -I m4
 
 # generate config.h.in from configure.ac
 autoheader --warnings=all --force
 
 # generate Makefile.in from Makefile.am and configure.ac
-automake-1.9 --warnings=all --add-missing --copy --force-missing
+automake-1.10 --warnings=all --add-missing --copy --force-missing
 
 # generate configure from configure.ac
 autoconf --warnings=all --force
