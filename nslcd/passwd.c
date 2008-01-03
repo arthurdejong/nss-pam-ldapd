95dfcb1b388e49ea65b4dbbadcedf528387cc364
Arthur de Jong <arthur@arthurdejong.org>
2008-01-03 10:30:30 +0000

    update copyright year
    git-svn-id: http://arthurdejong.org/svn/nss-pam-ldapd/nss-ldapd@555 ef36b2f9-881f-0410-afb5-c4e39611909c

--- nslcd/passwd.c
+++ nslcd/passwd.c
@@ -5,7 +5,7 @@
 
    Copyright (C) 1997-2005 Luke Howard
    Copyright (C) 2006 West Consulting
-   Copyright (C) 2006, 2007 Arthur de Jong
+   Copyright (C) 2006, 2007, 2008 Arthur de Jong
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
