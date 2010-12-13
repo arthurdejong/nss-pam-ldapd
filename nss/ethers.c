arthurdejong/nss-pam-ldapd

commit 90fec72d7d1390d0744622928c6648355d3f6f33

also try to support older Solaris releases that do not have the returnlen member of struct nss_XbyY_args
git-svn-id: http://arthurdejong.org/svn/nss-pam-ldapd/nss-pam-ldapd-solaris@1262 ef36b2f9-881f-0410-afb5-c4e39611909c

nss/ethers.c
@@ -96,6 +96,8 @@ nss_status_t _nss_ldap_endetherent(void)
 #define NSS_BUFLEN_ETHERS 1024
 #endif /* NSS_BUFLEN_ETHERS */
 
+#ifdef HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN
+
 static nss_status_t read_etherstring(TFILE *fp,nss_XbyY_args_t *args)
 {
   /* TODO: padl uses struct ether, verify */
@@ -129,8 +131,17 @@ static nss_status_t read_etherstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_etherent(fp,(struct etherent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_etherstring(fp,args); \
-  if (NSS_ARGS(args)->buf.result) \
-    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result
+  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
+
+#define READ_RESULT(fp) \
+  read_etherent(fp,(struct etherent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
+  if (retv=NSS_STATUS_SUCCESS) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 /* map a hostname to the corresponding ethernet address */
 static nss_status_t get_gethostton(nss_backend_t UNUSED(*be),void *args)

nss/group.c
@@ -183,6 +183,8 @@ nss_status_t _nss_ldap_initgroups_dyn(
 
 #ifdef NSS_FLAVOUR_SOLARIS
 
+#ifdef HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN
+
 static nss_status_t read_groupstring(TFILE *fp,nss_XbyY_args_t *args)
 {
   struct group result;
@@ -219,8 +221,17 @@ static nss_status_t read_groupstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_group(fp,(struct group *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_groupstring(fp,args); \
-  if (NSS_ARGS(args)->buf.result) \
-    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result
+  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
+
+#define READ_RESULT(fp) \
+  read_group(fp,(struct group *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
+  if (retv=NSS_STATUS_SUCCESS) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 static nss_status_t get_getgrnam_r(nss_backend_t UNUSED(*be),void *args)
 {

nss/hosts.c
@@ -242,6 +242,8 @@ nss_status_t _nss_ldap_endhostent(void)
 
 #ifdef NSS_FLAVOUR_SOLARIS
 
+#ifdef HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN
+
 static nss_status_t read_hoststring(TFILE *fp,nss_XbyY_args_t *args,int erronempty)
 {
   struct hostent result;
@@ -298,16 +300,29 @@ static nss_status_t read_hoststring(TFILE *fp,nss_XbyY_args_t *args,int erronemp
   NSS_ARGS(args)->buf.result? \
     read_hostent_erronempty(fp,NSS_ARGS(args)->key.hostaddr.type,(struct hostent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)): \
     read_hoststring(fp,args,1); \
-  if (NSS_ARGS(args)->buf.result) \
-    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result
+  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #define READ_RESULT_NEXTONEMPTY(fp) \
   NSS_ARGS(args)->buf.result? \
     read_hostent_nextonempty(fp,NSS_ARGS(args)->key.hostaddr.type,(struct hostent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)): \
     read_hoststring(fp,args,0); \
-  if (NSS_ARGS(args)->buf.result) \
-    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result
+  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
+#else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
+
+#define READ_RESULT_ERRONEMPTY(fp) \
+  read_hostent_erronempty(fp,NSS_ARGS(args)->key.hostaddr.type,(struct hostent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)); \
+  if (retv=NSS_STATUS_SUCCESS) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#define READ_RESULT_NEXTONEMPTY(fp) \
+  read_hostent_nextonempty(fp,NSS_ARGS(args)->key.hostaddr.type,(struct hostent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)); \
+  if (retv=NSS_STATUS_SUCCESS) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 /* hack to set the correct errno and h_errno */
 #define h_errnop &(NSS_ARGS(args)->h_errno)

nss/networks.c
@@ -162,6 +162,8 @@ nss_status_t _nss_ldap_endnetent(void)
 
 #ifdef NSS_FLAVOUR_SOLARIS
 
+#ifdef HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN
+
 static nss_status_t read_netentstring(TFILE *fp,nss_XbyY_args_t *args)
 {
   struct netent result;
@@ -199,8 +201,17 @@ static nss_status_t read_netentstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_netent(fp,(struct netent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)): \
     read_netentstring(fp,args); \
-  if (NSS_ARGS(args)->buf.result) \
-    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result
+  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
+
+#define READ_RESULT(fp) \
+  read_netent(fp,(struct netent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)); \
+  if (retv=NSS_STATUS_SUCCESS) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 /* more of a dirty hack */
 #define h_errnop (&(NSS_ARGS(args)->h_errno))

nss/passwd.c
@@ -97,6 +97,8 @@ nss_status_t _nss_ldap_endpwent(void)
 
 #ifdef NSS_FLAVOUR_SOLARIS
 
+#ifdef HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN
+
 static nss_status_t read_passwdstring(TFILE *fp,nss_XbyY_args_t *args)
 {
   struct passwd result;
@@ -127,8 +129,17 @@ static nss_status_t read_passwdstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_passwd(fp,(struct passwd *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_passwdstring(fp,args); \
-  if (NSS_ARGS(args)->buf.result) \
-    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result
+  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
+
+#define READ_RESULT(fp) \
+  read_passwd(fp,(struct passwd *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
+  if (retv=NSS_STATUS_SUCCESS) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 static nss_status_t get_getpwnam(nss_backend_t UNUSED(*be),void *args)
 {

nss/protocols.c
@@ -93,6 +93,8 @@ nss_status_t _nss_ldap_endprotoent(void)
 
 #ifdef NSS_FLAVOUR_SOLARIS
 
+#ifdef HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN
+
 static nss_status_t read_protostring(TFILE *fp,nss_XbyY_args_t *args)
 {
   struct protoent result;
@@ -128,8 +130,17 @@ static nss_status_t read_protostring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_protoent(fp,(struct protoent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_protostring(fp,args); \
-  if (NSS_ARGS(args)->buf.result) \
-    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result
+  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
+
+#define READ_RESULT(fp) \
+  read_protoent(fp,(struct protoent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
+  if (retv=NSS_STATUS_SUCCESS) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 static nss_status_t get_getprotobyname(nss_backend_t UNUSED(*be),void *args)
 {

nss/rpc.c
@@ -93,6 +93,8 @@ nss_status_t _nss_ldap_endrpcent(void)
 
 #ifdef NSS_FLAVOUR_SOLARIS
 
+#ifdef HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN
+
 static nss_status_t read_rpcstring(TFILE *fp,nss_XbyY_args_t *args)
 {
   struct rpcent result;
@@ -128,8 +130,17 @@ static nss_status_t read_rpcstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_rpcent(fp,(struct rpcent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_rpcstring(fp,args); \
-  if (NSS_ARGS(args)->buf.result) \
-    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result
+  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
+
+#define READ_RESULT(fp) \
+  read_rpcent(fp,(struct rpcent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
+  if (retv=NSS_STATUS_SUCCESS) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 static nss_status_t get_getrpcbyname_r(nss_backend_t UNUSED(*be),void *args)
 {

nss/services.c
@@ -97,6 +97,8 @@ nss_status_t _nss_ldap_endservent(void)
 
 #ifdef NSS_FLAVOUR_SOLARIS
 
+#ifdef HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN
+
 static nss_status_t read_servstring(TFILE *fp,nss_XbyY_args_t *args)
 {
   struct servent result;
@@ -132,8 +134,17 @@ static nss_status_t read_servstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_servent(fp,(struct servent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_servstring(fp,args); \
-  if (NSS_ARGS(args)->buf.result) \
-    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result
+  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
+
+#define READ_RESULT(fp) \
+  read_servent(fp,(struct servent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
+  if (retv=NSS_STATUS_SUCCESS) \
+    NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
+
+#endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 static nss_status_t get_getservbyname_r(nss_backend_t UNUSED(*be),void *args)
 {

commit 85eefd6f9e2f384f7df2d5c503eb3a75f15bb53f

fix assignment in if statement (was broken in r1262)
git-svn-id: http://arthurdejong.org/svn/nss-pam-ldapd/nss-pam-ldapd-solaris@1330 ef36b2f9-881f-0410-afb5-c4e39611909c

nss/ethers.c
@@ -131,14 +131,14 @@ static nss_status_t read_etherstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_etherent(fp,(struct etherent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_etherstring(fp,args); \
-  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+  if ((NSS_ARGS(args)->buf.result)&&(retv==NSS_STATUS_SUCCESS)) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 #define READ_RESULT(fp) \
   read_etherent(fp,(struct etherent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
-  if (retv=NSS_STATUS_SUCCESS) \
+  if (retv==NSS_STATUS_SUCCESS) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */

nss/group.c
@@ -221,14 +221,14 @@ static nss_status_t read_groupstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_group(fp,(struct group *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_groupstring(fp,args); \
-  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+  if ((NSS_ARGS(args)->buf.result)&&(retv==NSS_STATUS_SUCCESS)) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 #define READ_RESULT(fp) \
   read_group(fp,(struct group *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
-  if (retv=NSS_STATUS_SUCCESS) \
+  if (retv==NSS_STATUS_SUCCESS) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */

nss/hosts.c
@@ -300,26 +300,26 @@ static nss_status_t read_hoststring(TFILE *fp,nss_XbyY_args_t *args,int erronemp
   NSS_ARGS(args)->buf.result? \
     read_hostent_erronempty(fp,NSS_ARGS(args)->key.hostaddr.type,(struct hostent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)): \
     read_hoststring(fp,args,1); \
-  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+  if ((NSS_ARGS(args)->buf.result)&&(retv==NSS_STATUS_SUCCESS)) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #define READ_RESULT_NEXTONEMPTY(fp) \
   NSS_ARGS(args)->buf.result? \
     read_hostent_nextonempty(fp,NSS_ARGS(args)->key.hostaddr.type,(struct hostent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)): \
     read_hoststring(fp,args,0); \
-  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+  if ((NSS_ARGS(args)->buf.result)&&(retv==NSS_STATUS_SUCCESS)) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 #define READ_RESULT_ERRONEMPTY(fp) \
   read_hostent_erronempty(fp,NSS_ARGS(args)->key.hostaddr.type,(struct hostent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)); \
-  if (retv=NSS_STATUS_SUCCESS) \
+  if (retv==NSS_STATUS_SUCCESS) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #define READ_RESULT_NEXTONEMPTY(fp) \
   read_hostent_nextonempty(fp,NSS_ARGS(args)->key.hostaddr.type,(struct hostent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)); \
-  if (retv=NSS_STATUS_SUCCESS) \
+  if (retv==NSS_STATUS_SUCCESS) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */

nss/networks.c
@@ -201,14 +201,14 @@ static nss_status_t read_netentstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_netent(fp,(struct netent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)): \
     read_netentstring(fp,args); \
-  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+  if ((NSS_ARGS(args)->buf.result)&&(retv==NSS_STATUS_SUCCESS)) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 #define READ_RESULT(fp) \
   read_netent(fp,(struct netent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno,&(NSS_ARGS(args)->h_errno)); \
-  if (retv=NSS_STATUS_SUCCESS) \
+  if (retv==NSS_STATUS_SUCCESS) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */

nss/passwd.c
@@ -129,14 +129,14 @@ static nss_status_t read_passwdstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_passwd(fp,(struct passwd *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_passwdstring(fp,args); \
-  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+  if ((NSS_ARGS(args)->buf.result)&&(retv==NSS_STATUS_SUCCESS)) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 #define READ_RESULT(fp) \
   read_passwd(fp,(struct passwd *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
-  if (retv=NSS_STATUS_SUCCESS) \
+  if (retv==NSS_STATUS_SUCCESS) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */

nss/protocols.c
@@ -130,14 +130,14 @@ static nss_status_t read_protostring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_protoent(fp,(struct protoent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_protostring(fp,args); \
-  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+  if ((NSS_ARGS(args)->buf.result)&&(retv==NSS_STATUS_SUCCESS)) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 #define READ_RESULT(fp) \
   read_protoent(fp,(struct protoent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
-  if (retv=NSS_STATUS_SUCCESS) \
+  if (retv==NSS_STATUS_SUCCESS) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */

nss/rpc.c
@@ -130,14 +130,14 @@ static nss_status_t read_rpcstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_rpcent(fp,(struct rpcent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_rpcstring(fp,args); \
-  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+  if ((NSS_ARGS(args)->buf.result)&&(retv==NSS_STATUS_SUCCESS)) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 #define READ_RESULT(fp) \
   read_rpcent(fp,(struct rpcent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
-  if (retv=NSS_STATUS_SUCCESS) \
+  if (retv==NSS_STATUS_SUCCESS) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */

nss/services.c
@@ -134,14 +134,14 @@ static nss_status_t read_servstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_servent(fp,(struct servent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_servstring(fp,args); \
-  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+  if ((NSS_ARGS(args)->buf.result)&&(retv==NSS_STATUS_SUCCESS)) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 #define READ_RESULT(fp) \
   read_servent(fp,(struct servent *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
-  if (retv=NSS_STATUS_SUCCESS) \
+  if (retv==NSS_STATUS_SUCCESS) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */

nss/shadow.c
@@ -155,14 +155,14 @@ static nss_status_t read_spwdstring(TFILE *fp,nss_XbyY_args_t *args)
   NSS_ARGS(args)->buf.result? \
     read_spwd(fp,(struct spwd *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno): \
     read_spwdstring(fp,args); \
-  if ((NSS_ARGS(args)->buf.result)&&(retv=NSS_STATUS_SUCCESS)) \
+  if ((NSS_ARGS(args)->buf.result)&&(retv==NSS_STATUS_SUCCESS)) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #else /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */
 
 #define READ_RESULT(fp) \
   read_spwd(fp,(struct spwd *)NSS_ARGS(args)->buf.result,NSS_ARGS(args)->buf.buffer,NSS_ARGS(args)->buf.buflen,&errno); \
-  if (retv=NSS_STATUS_SUCCESS) \
+  if (retv==NSS_STATUS_SUCCESS) \
     NSS_ARGS(args)->returnval=NSS_ARGS(args)->buf.result;
 
 #endif /* not HAVE_STRUCT_NSS_XBYY_ARGS_RETURNLEN */