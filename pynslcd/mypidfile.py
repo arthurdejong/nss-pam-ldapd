5936744757e06ceea117086457d082e8befb6233
Arthur de Jong <arthur@arthurdejong.org>
2011-03-24 22:09:08 +0000

    truncate pidfile to ensure remains of previous value is gone
    git-svn-id: http://arthurdejong.org/svn/nss-pam-ldapd/nss-pam-ldapd@1407 ef36b2f9-881f-0410-afb5-c4e39611909c

--- pynslcd/mypidfile.py
+++ pynslcd/mypidfile.py
@@ -1,7 +1,7 @@
 
 # mypidfile.py - functions for properly locking a PIDFile
 #
-# Copyright (C) 2010 Arthur de Jong
+# Copyright (C) 2010, 2011 Arthur de Jong
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -40,6 +40,7 @@ class MyPIDLockFile(object):
             os.close(fd)
             raise
         pidfile.write('%d\n' % os.getpid())
+        pidfile.truncate()
         pidfile.flush()
         self.pidfile = pidfile
         return self
