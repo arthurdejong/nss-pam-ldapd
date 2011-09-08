373a1535130bc18ddcb3da38c9b429149756669b
Arthur de Jong <arthur@arthurdejong.org>
2011-09-08 20:57:09 +0000

    grow gecos buffer size and consistency improvements to other buffers
    git-svn-id: http://arthurdejong.org/svn/nss-pam-ldapd/nss-pam-ldapd@1540 ef36b2f9-881f-0410-afb5-c4e39611909c

--- nslcd/passwd.c
+++ nslcd/passwd.c
@@ -442,9 +442,9 @@ static int write_passwd(TFILE *fp,MYLDAP_ENTRY *entry,const char *requser,
   int numuids;
   char gidbuf[32];
   gid_t gid;
-  char gecos[100];
-  char homedir[100];
-  char shell[100];
+  char gecos[1024];
+  char homedir[256];
+  char shell[64];
   char passbuffer[64];
   int i,j;
   /* get the usernames for this entry */
