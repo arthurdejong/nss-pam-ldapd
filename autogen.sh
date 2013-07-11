5b785086084122074dea451850aa87e2d7594c77
Arthur de Jong <arthur@arthurdejong.org>
2013-07-11 21:48:35 +0200

    Upgrade to automake 1.13

--- autogen.sh
+++ autogen.sh
@@ -7,25 +7,21 @@
   cp -f /usr/share/misc/config.sub config.sub
 [ -r /usr/share/misc/config.guess ] && \
   cp -f /usr/share/misc/config.guess config.guess
-[ -r /usr/share/automake-1.11/mkinstalldirs ] && \
-  cp -f /usr/share/automake-1.11/mkinstalldirs mkinstalldirs
-[ -r /usr/share/automake-1.11/missing ] && \
-  cp -f /usr/share/automake-1.11/missing missing
-[ -r /usr/share/automake-1.11/install-sh ] && \
-  cp -f /usr/share/automake-1.11/install-sh install-sh
-[ -r /usr/share/automake-1.11/depcomp ] && \
-  cp -f /usr/share/automake-1.11/depcomp depcomp
-[ -r /usr/share/automake-1.11/INSTALL ] && \
-  cp -f /usr/share/automake-1.11/INSTALL INSTALL
+for fname in INSTALL ar-lib compile depcomp install-sh missing \
+             mkinstalldirs py-compile test-driver
+do
+  [ -r /usr/share/automake-1.13/$fname ] && \
+    cp -f /usr/share/automake-1.13/$fname $fname
+done
 
 # generate aclocal.m4 from configure.ac
-aclocal-1.11 -I m4
+aclocal -I m4
 
 # generate config.h.in from configure.ac
 autoheader --warnings=all --force
 
 # generate Makefile.in from Makefile.am and configure.ac
-automake-1.11 --warnings=all --add-missing --copy --force-missing
+automake --warnings=all --add-missing --copy --force-missing
 
 # generate configure from configure.ac
 autoconf --warnings=all --force
