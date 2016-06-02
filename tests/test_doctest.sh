fd61bb665b285a51ced803e6ab3164671054c694
Arthur de Jong <arthur@arthurdejong.org>
2016-06-02 23:22:47 +0200

    Add test for running doctests

---
+++ tests/test_doctest.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# test_doctest.sh - run Python doctests
+#
+# Copyright (C) 2016 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+set -e
+
+# find source directory
+srcdir="${srcdir-`dirname "$0"`}"
+top_srcdir="${top_srcdir-${srcdir}/..}"
+python="${PYTHON-python}"
+
+# if Python is missing, ignore
+if ! ${python} --version > /dev/null 2> /dev/null
+then
+  echo "Python (${python}) not found"
+  exit 77
+fi
+
+# run doctests
+for dir in "$top_srcdir"/pynslcd "$top_srcdir"/utils
+do
+  echo "Running doctests in `basename $dir`..."
+  (cd "$dir"; ${python} -m doctest -v *.py)
+done
