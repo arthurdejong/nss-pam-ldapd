2f6e65ab384917a9b2ee4b25a172acc107a640b3
Arthur de Jong <arthur@arthurdejong.org>
2022-10-10 23:16:15 +0200

    Use closefrom() if available
    One some systems _SC_OPEN_MAX can be *very* large.
    
    Closes https://github.com/arthurdejong/nss-pam-ldapd/issues/53

--- nslcd/daemonize.c
+++ nslcd/daemonize.c
@@ -1,7 +1,7 @@
 /*
    daemoninze.c - functions for properly daemonising an application
 
-   Copyright (C) 2014-2015 Arthur de Jong
+   Copyright (C) 2014-2022 Arthur de Jong
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -43,8 +43,11 @@ static int daemonizefd = -1;
 
 void daemonize_closefds(void)
 {
-  int i;
   /* close all file descriptors (except stdin/out/err) */
+#ifdef HAVE_CLOSEFROM
+  closefrom(3);
+#else
+  int i;
   i = sysconf(_SC_OPEN_MAX) - 1;
   /* if the system does not have OPEN_MAX just close the first 32 and
      hope we closed enough */
@@ -52,6 +55,7 @@ void daemonize_closefds(void)
     i = 32;
   for (; i > 2; i--)
     close(i);
+#endif
 }
 
 void daemonize_redirect_stdio(void)
