e9662f13ac7a2a5d0e3eb8864dc7cf2a9ceb0a79
Brett Lymn <blymn@internode.on.net>
2023-05-30 07:52:10 +0930

    Fix compilation issues on NetBSD
    Fixes da63099

--- compat/getpeercred.c
+++ compat/getpeercred.c
@@ -101,14 +101,9 @@ int getpeercred(int sock, uid_t *uid, gid_t *gid, pid_t *pid)
   if (uid == NULL)
     uid = &tuid;
   if (gid == NULL)
-    gid = &tguid;
+    gid = &tgid;
   if (getpeereid(sock, uid, gid))
     return -1;
-  /* return the data */
-  if (uid != NULL)
-    *uid = cred.uid;
-  if (gid != NULL)
-    *gid = cred.gid;
   if (pid != NULL)
     *pid = -1; /* we return a -1 pid because we have no usable pid */
   return 0;
